e9a35530bce2bb50bd88c73865878f55e0952dd0
thiagocantero <thiagocantero@gmail.com>
2025-04-05 15:55:17 -0300

    Create Models: Rastreamento, Cliente, Frete and handle variables, in Rastreamento Controller, from database and show dynamically on the Component Rastreamento

--- app/Http/Controllers/RastreamentoController.php
+++ app/Http/Controllers/RastreamentoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Frete;
 use Illuminate\Http\Request;
 
 class RastreamentoController extends Controller
@@ -11,6 +12,11 @@ class RastreamentoController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('frete.Rastreamento');
+        $frete = Frete::where('codigo_rastreio', 'TESTE123')->first();
+
+
+        return view('frete.Rastreamento', [
+            'frete' => $frete
+        ]);
     }
 }
