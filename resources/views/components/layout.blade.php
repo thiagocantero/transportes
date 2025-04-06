66b4956b6a0d4dca78a9393630942309fb14b682
thiagocantero <thiagocantero@gmail.com>
2025-04-06 17:47:57 -0300

    Exception case register not found

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,6 +21,17 @@
             </div>
         </nav>
 
+        @if (session('error'))
+            <div class="container mx-auto px-4 pt-20">
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 roundend relative" role="alert">
+                    <strong class="font-bold">Erro!</strong>
+                    <spang class="block sm:inline">{{session('error')}}</span>
+                    </div>
+                </div>            
+        @endif        
+
+
+
        <div class="container mx-auto px-4 pt-16">
             {{ $slot }}
        </div>
