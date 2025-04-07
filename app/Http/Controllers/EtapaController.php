4ee2a5ee661ccc2639b1ca89d80916c451c8c35a
thiagocantero <thiagocantero@gmail.com>
2025-04-07 10:41:40 -0300

    Apply Business rules in Etapas, implements on ENUMS function and Controller Etapa, also implements in Validation Rule

--- app/Http/Controllers/EtapaController.php
+++ app/Http/Controllers/EtapaController.php
@@ -2,16 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\FreteStatus;
 use App\Http\Requests\StoreEtapaRequest;
 use App\Models\Etapa;
-
+use App\Models\Frete;
 
 class EtapaController extends Controller
 {
     public function Store(StoreEtapaRequest $request)
     {
+        $frete = Frete::find($request->frete_id);
+
+        if ($frete->status == FreteStatus::ENTREGUE) {
+            return response()->json([
+                'message' => 'Não é possível adicionar Etapas ao Frete que já foi Entregue.'
+            ], 400);
+        }
+
 
         $etapa = Etapa::create($request->all());
+        $tipoFreteStatus = FreteStatus::fromName($request->tipo_etapa);
+        $frete->update(['status' => $tipoFreteStatus]);
+
 
         return $etapa;
     }
