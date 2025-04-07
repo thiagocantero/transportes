87a7072a33765b67d4a2c1c453efad1f5bb7c228
thiagocantero <thiagocantero@gmail.com>
2025-04-07 17:40:38 -0300

    Ft Relationship Manager Frete between Etapas

--- app/Filament/Resources/FreteResource.php
+++ app/Filament/Resources/FreteResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\FreteResource\Pages;
 use App\Filament\Resources\FreteResource\RelationManagers;
+use App\Filament\Resources\FreteResource\RelationManagers\EtapasRelationManager;
 use App\Models\Frete;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -96,7 +97,7 @@ class FreteResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            EtapasRelationManager::class
         ];
     }
 
